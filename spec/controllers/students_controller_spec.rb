81a5952a79c4dbc8d2ca78c1a4386cf9a343f183
jbharwood <josephblakeharwood@gmail.com>
2019-02-04 10:44:50 -0500

    Done.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 describe StudentsController do
 
+  if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
   describe "POST create" do
     context "with valid attributes" do
       it "creates a new student" do
